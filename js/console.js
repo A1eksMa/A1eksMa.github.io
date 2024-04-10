41308f02fb0f831707a86e558130d51b9291f6b1
A1ex Ma <145852894+A1eksMa@users.noreply.github.com>
2024-04-11 02:49:04 +0600

    Update console.js

--- js/console.js
+++ js/console.js
@@ -16,7 +16,7 @@ var aText = new Array(
 "> ",
 "> python3",
 ">>> print(\n.join( .join(*zip(*row)) for row in ([[* if row==0 and col%3!=0 or row==1 and col%3==0 or row-col==2 or row+col==8 else   for col in range(7)] for row in range(6)])))",
-">>> ________* *___* *________",
+">>> _________* *___* *_______",
 ">>> ______*_____*_____*______",
 ">>> ______*___________*______",
 ">>> ________*_______*________",
